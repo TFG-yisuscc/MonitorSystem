d4ea13d5bd74b4d5441d92acb6c90998bef3671c
Jesús Carrascosa Carro <jescarcar5@alum.us.es>
2025-03-10 17:23:41 +0100

    añadido mpstat
    Signed-off-by: Jesús Carrascosa Carro <jescarcar5@alum.us.es>

--- Metrics/VGENMetrics.py
+++ Metrics/VGENMetrics.py
@@ -4,7 +4,7 @@ import subprocess
 #this class stores metrics related to CPU atribbutes
 class VCGENMetrics:
     #constructor 
-    def __init__():
+    def __init__(self):
         #temperatura  CPU
         cmd_output = subprocess.check_output(["vcgencmd", "measure_temp"]).decode("utf-8")
         self.temperature = float(cmd_output.split("=")[1][:-3]) #TODO comprobar que funciona 
@@ -21,10 +21,10 @@ class VCGENMetrics:
         cmd_output = subprocess.check_output(["vcgencmd", "get_mem", "arm"]).decode("utf-8")
         self.memory = int(cmd_output.split("=")[1][:-1])
 # to string
-def __str__():
-    return f"Temperature: {self.temperature}, Frequency: {self.frequency}, Voltage: {self.voltage}, Throttling: {self.throttling}, Memory: {self.memory}"
+    def __str__(self):
+        return f"Temperature: {self.temperature}, Frequency: {self.frequency}, Voltage: {self.voltage}, Throttling: {self.throttling}, Memory: {self.memory}"
 # update
-def update():
+    def update(self):
             #temperatura  CPU
         cmd_output = subprocess.check_output(["vcgencmd", "measure_temp"]).decode("utf-8")
         self.temperature = float(cmd_output.split("=")[1][:-3]) #TODO comprobar que funciona 
@@ -40,4 +40,4 @@ def update():
         #memoria
         cmd_output = subprocess.check_output(["vcgencmd", "get_mem", "arm"]).decode("utf-8")
         self.memory = int(cmd_output.split("=")[1][:-1])
-#
+
